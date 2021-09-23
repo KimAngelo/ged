ee959ad2e5d15dbfd0e0873102a8e4f1045cfc8e
Kim Angelo <kimangelo.jow@gmail.com>
2021-09-23 11:54:02 -0300

    retirado marca da empresa do rodape

--- themes/base/views/footer.php
+++ themes/base/views/footer.php
@@ -2,11 +2,11 @@
     <!--begin::Container-->
     <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Copyright-->
-        <div class="text-dark order-2 order-md-1">
+       <!-- <div class="text-dark order-2 order-md-1">
             <span class="text-muted font-weight-bold mr-2">Desenvolvido por </span>
             <a href="https://g7company.com.br/programador" target="_blank"
                class="text-dark-75 text-hover-primary">G7 Company</a>
-        </div>
+        </div>-->
         <!--end::Copyright-->
 
     </div>
